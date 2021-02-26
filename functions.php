38c8941b9b753b265cf9da4af1c1bf9072a17c0f
Yeasiin <yeasinarfatemon12345@gmail.com>
2021-02-26 17:40:14 +0600

    Catagories Added

--- functions.php
+++ functions.php
@@ -1,27 +1,68 @@
 <?php
-    require_once "DB/config.php";
+require_once "DB/config.php";
+session_start();
+$action = $_POST["action"] ?? "";
 
-if(isset($_POST["adminLogin"])){
-
+if ("login" == $action) {
     $email = $_POST["email"];
     $password = $_POST["password"];
     $query = "SELECT * FROM admin_login WHERE email='{$email}'";
     $result = mysqli_query($connection, $query);
 
-    if(mysqli_num_rows($result) > 0 ){
+    if (mysqli_num_rows($result) > 0) {
         $data = mysqli_fetch_assoc($result);
         $_password = $data["password"];
-        if(password_verify($password, $_password)){
-            header("location:home.php");
+        if (password_verify($password, $_password)) {
+            $_SESSION["id"] = $data["id"];
+            $_SESSION["name"] = $data["name"];
+            header("location:dashboard.php");
             die();
-        }else{
-            
         };
+    }
+    header("location:adminLogin.php");
+} elseif ("addCatagorie" == $action) {
+    $catagorieName = htmlspecialchars(trim($_POST["catagoriesName"]));
+    $catagorieDescription = htmlspecialchars($_POST["catagorieDescription"]);
+
+    $sQuery = "SELECT * FROM catagories WHERE catagories_name=\"{$catagorieName}\"";
+    $sResult = mysqli_query($connection, $sQuery);
+
+    if (mysqli_num_rows($sResult) < 1) {
+
+        $query = "INSERT INTO catagories (catagories_name , catagories_description) VALUES(\"$catagorieName\" ,\"$catagorieDescription\")";
+        $result = mysqli_query($connection, $query);
+        if (mysqli_connect_errno()) {
+            echo "Failed to connect to MySQL: " . mysqli_connect_error();
+            exit();
+        } else {
+            header("location:catagories.php");
+        }
     }else{
-        header("location:adminLogin.php");
-    };
+        header("location:catagories.php");
 
+    }
+} elseif ("Update" == $action) {
+    $catagorieName = htmlspecialchars($_POST["catagoriesName"]);
+    $catagorieDescription = htmlspecialchars($_POST["catagorieDescription"]);
+    $id = htmlspecialchars($_POST["id"]);
 
-
-    
+    $query = "UPDATE catagories
+    SET catagories_name=\"{$catagorieName}\", catagories_description=\"{$catagorieDescription}\" WHERE id={$id}";
+    $result = mysqli_query($connection, $query);
+    if (mysqli_connect_errno()) {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        exit();
+    } else {
+        header("location:catagories.php");
+    }
+} elseif ("Delete" == $action) {
+    $id = htmlspecialchars($_POST["id"]);
+    $query = "DELETE FROM catagories WHERE id=\"{$id}\" ";
+    mysqli_query($connection, $query);
+    if (mysqli_connect_errno()) {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        exit();
+    } else {
+        header("location:catagories.php");
+    }
 };
