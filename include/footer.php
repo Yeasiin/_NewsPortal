56fc4ea514f5cbb080d008e05420e2ab8e407316
Yeasiin <yeasinarfatemon12345@gmail.com>
2021-03-07 00:46:13 +0600

    Single Blog News

--- include/footer.php
+++ include/footer.php
@@ -3,8 +3,6 @@
 
     <!-- Icons -->
     <script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
-    <!-- Graphs -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js"></script>
     <script src="js/app.js"></script>
 
   </body>
---
+++ singleNews.php
@@ -0,0 +1,48 @@
+<?php
+require_once "include/header.php";
+require_once "include/blogNavigation.php";
+
+$blogId = $_GET["id"] ?? "";
+
+
+$query = "SELECT * FROM news WHERE id=\"{$blogId}\" ";
+$result = mysqli_query($connection, $query);
+$indexNews = mysqli_fetch_assoc($result);
+
+?>
+
+
+<div class="blog-post">
+          <h2 class="blog-post-title"><?php echo $indexNews["newsTitle"]; ?></h2>
+          <p class="blog-post-meta"><?php echo date("F j, Y", strtotime($indexNews["createdAt"])); ?> by <a class="text-capitalize" href="#"><?php echo $indexNews["createdBy"] ?></a></p>
+          <p><img src="<?php echo $indexNews["newsThumbnail"]; ?>" width="100%" alt=""></p>
+          <p>
+            <?php
+            echo $indexNews["newsDescription"];
+            ?>
+
+          </p>
+        </div><!-- /.blog-post -->
+
+
+
+  </div><!-- /.row -->
+
+</div><!-- /.container -->
+
+<footer class="blog-footer">
+  <p>Blog template built for <a href="https://getbootstrap.com/">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+  <p>
+    <a href="#" >Back to top</a>
+  </p>
+</footer>
+
+
+</body>
+
+</html>
+<?php
+function headerTitle(){
+    
+}
+?>
